4a9ffd5dfb38ffb8e98e6a8ea075d569923ea6e9
Afeez Awoyemi <damafeez@gmail.com>
2018-07-17 22:09:12 +0100

    Add Button Scroll Functionality Add SVG icons To Input Field Add SVG icons to Footer Buttons

--- UI/js/app.js
+++ UI/js/app.js
@@ -1,5 +1,5 @@
 let initInput = () => {
-  let inputFields = document.querySelectorAll('.input > input')
+  let inputFields = document.querySelectorAll('.input > input, .textarea > textarea')
   if (inputFields.length === 0) return
   for (inputField of inputFields) {
     inputField.addEventListener('focus', function () {
@@ -14,14 +14,11 @@ let toggleView = (view) => {
   let authContainer = document.querySelector('section.auth')
   authContainer.className = `auth ${view}-view`
 }
-let scroller = document.querySelector('div.scroller')
-let main = document.querySelector('.logged-in main')
-let diaries = document.querySelector('section.diaries')
-let setTransform = () => {
-  let mainStyle = main.currentStyle || window.getComputedStyle(main)
-  let offset = -(diaries.offsetLeft - mainStyle.paddingLeft.match(/\d+/g)[0])
-  console.log({offset})
-  scroller.style.transform = 'translate3d(' + (offset) + 'px,0,0)';
+let slideTo = (val) => {
+  let scroller = document.querySelector('div.scroller')
+  let diaries = document.querySelector('section.diaries')
+  let transform = val === 0 ? val : -diaries.offsetLeft
+  scroller.style.transform = `translate3d(${transform}px, 0, 0)`;
 }
 let navigateTo = (url) => {
   window.location.assign = url
