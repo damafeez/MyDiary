26b8007df44fdb92070656cd09ff48581e8a15d8
Afeez Awoyemi <damafeez@gmail.com>
2018-07-19 19:47:24 +0100

    Add POST /entries - POST /entries test passing

--- server/routes/diary.js
+++ server/routes/diary.js
@@ -3,6 +3,20 @@ import Diary from '../model/Diary';
 
 const router = express.Router();
 
+router.post('/entries', async (req, res) => {
+  try {
+    const { title, body, author } = req.body;
+    if (title && body && author) {
+      const newDiary = new Diary({ title, body, author });
+      const diary = await Diary.save(newDiary);
+      res.json(diary);
+    } else {
+      throw new Error('all fields must be provided');
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
 router.get('/entries', async (req, res) => {
   try {
     const entries = await Diary.find();
