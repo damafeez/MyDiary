4890b8493d07dfe752697fc871069f03e542bd25
Afeez Awoyemi <damafeez@gmail.com>
2018-07-19 11:53:32 +0100

    Add route / to test server availability

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-// this will contain the server
+import express from 'express';
+
+const PORT = process.env.PORT || 3030;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Server is live');
+});
+
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+
+export default app;
